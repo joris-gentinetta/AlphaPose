825c7885220980f3181ed37fc8a6b7c087a5d396
Martin <1500595+bmmtstb@users.noreply.github.com>
2023-11-22 13:10:09 +0100

    yolo and tracker api use local paths

--- alphapose/version.py
+++ alphapose/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Thu Jul 21 17:10:51 2022
+# TIME: Fri Nov 10 17:46:57 2023
 
-__version__ = '0.6.0+29ace8c'
-short_version = '0.6.0'
+__version__ = '0.5.0+8a8bd3c'
+short_version = '0.5.0'
